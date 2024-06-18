0a10ac3fb790e722b488da4eca360d816ee45969
Kate <ekaterina2002aleks@gmail.com>
2024-06-18 22:49:54 +0300

    Last changes

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,17 +1,21 @@
 import { Routes, Route, HashRouter } from "react-router-dom";
-// import { HomePage } from "./components/HomePage/HomePage";
-// import App from "./App";
-import { Admin } from "./components/Admin"
+import { HomePage } from "./components/HomePage/HomePage";
+import App from "./App";
+import { AdminCoursesPreview } from "./components/AdminCoursesPreview"
+import { AdminCoursesEdit } from "./components/AdminCoursesEdit";
 
 export const Root = () => {
   return (
     <HashRouter>
       <Routes>
-        {/* <Route path="/" element={<App />} >
+        <Route path="/" element={<App />} >
           <Route index element={<HomePage />} />
 
-        </Route> */}
-        <Route path="/" element={<Admin />} > {/* admin/courses/preview */}
+        </Route>
+        <Route path="/preview" element={<AdminCoursesPreview />} > {/* admin/courses/preview */}
+        </Route>
+
+        <Route path="/edit" element={<AdminCoursesEdit />} > {/* admin/courses/edit */}
         </Route>
         
       </Routes>
