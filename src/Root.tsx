f107c975e0773bf7c6b3c8a2e1c6d850dbf5a518
Rodionsav <rodionsav@mate.academy>
2024-06-23 22:17:48 +0200

    new pages

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,17 +1,19 @@
 import { Routes, Route, HashRouter } from "react-router-dom";
-// import { HomePage } from "./components/HomePage/HomePage";
-// import App from "./App";
-import { Admin } from "./components/Admin"
+import { HomePage } from "./components/HomePage/HomePage";
+import App from "./App";
+import { CoursesPage } from "./components/CoursesPage/CoursesPage";
+import { BlogPage } from "./components/BlogPage/BlogPage";
+import { SuccessPage } from "./components/SuccessPage/SuccessPage";
 
 export const Root = () => {
   return (
     <HashRouter>
       <Routes>
-        {/* <Route path="/" element={<App />} >
+        <Route path="/" element={<App />} >
           <Route index element={<HomePage />} />
-
-        </Route> */}
-        <Route path="/" element={<Admin />} > {/* admin/courses/preview */}
+          <Route path="courses" element={<CoursesPage />} />
+          <Route path="blog" element={<BlogPage />} />
+          <Route path="success" element={<SuccessPage />} />
         </Route>
         
       </Routes>
--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,49 +1,56 @@
 import '../../styles/page.scss';
 import './home.scss';
-import './advantages.scss';
+// import './advantages.scss';
 import './history.scss';
 import '../../styles/form.scss';
 import { MenuSlider } from '../MenuSlider/MenuSlider';
 import { CourseCard } from '../CourseCard/CourseCard';
+import { AdvantagesSlider } from '../AdvantagesSlider/AdvantagesSlider';
 
 export const HomePage = () => {
   return (
     <section className='home'>
       <MenuSlider />
-      <article className='advantages'>
-        <img src='images/graduates-200-img.png' className='advantages-img advantages-img-first' />
-        <img src='images/get-promotions-img.png' className='advantages-img advantages-img-second' />
-        <img src='images/free-maditations-img.png' className='advantages-img advantages-img-third' />
-      </article>
+      <AdvantagesSlider />
       <article className='consultation'>
-        <h1 className='consultation__title'>If you don't know which course to choose, get a consultation.</h1>
-        <button className='consultation__button page__button-secondary'>
-          Get free
-        </button>
+        <div className='consultation-container'>
+          <h1 className='consultation__title'>If you don't know which course to choose, get a consultation.</h1>
+          <button className='consultation__button page__button-secondary'>
+            Get free
+          </button>
+        </div>
       </article>
       <article className='course'>
         <h1 className='page__title course__title'>Courses</h1>
-        <CourseCard />
+        <div className='course-container'>
+          <CourseCard />
+          <CourseCard />
+        </div>
       </article>
       <article className='history'>
         <h1 className='page__title_medium history__title'>History of success</h1>
-        <div className='history-container'>
-          <div>
-            <p className='history__paragraph'>
-              I have become more confident in my speech, minimized the use of words-parasites, and finally understood how to maintain the right pace and make pauses effectively.
-              I witnessed not only my own progress but also the progress of my peers. I was genuinely happy for their successes and admired each of their presentations...
-            </p>
-            <div className='history-img-container'>
-              <img src='images/telegram-icon-img.svg' />
-              <img src='images/instagram-icon-secondary-img.svg' />
+        <div
+          // className='history-container'
+          className='history__main-container'
+        >
+          <div className='history-container'>
+            <div className='history__paragraph-container'>
+              <p className='history__paragraph'>
+                I have become more confident in my speech, minimized the use of words-parasites, and finally understood how to maintain the right pace and make pauses effectively.
+                I witnessed not only my own progress but also the progress of my peers. I was genuinely happy for their successes and admired each of their presentations...
+              </p>
+              <div className='history-img-container'>
+                <img src='images/telegram-icon-img.svg' />
+                <img src='images/instagram-icon-secondary-img.svg' />
+              </div>
+              <div className='history__button-container'>
+                <button className='page__button-secondary history__button'>
+                  Read more
+                </button>
+              </div>
             </div>
-            <div className='history__button-container'>
-              <button className='page__button-secondary history__button'>
-                Read more
-              </button>
-            </div>
+            <img src='images/woman-history-img.svg' className='history__main-img' />
           </div>
-          <img src='images/woman-history-img.svg' />
         </div>
       </article>
       <article className='animals'>
@@ -66,47 +73,6 @@ export const HomePage = () => {
           </h3>
         </div>
       </article>
-      <form className='form'>
-        <div className='form-container'>
-          <h1 className='form__title'>Do you need additional consultation?</h1>
-          <div className='form__field-container'>
-            <label className='form__label'>
-              <span className='form__label__title'>Name</span>
-              <input
-                type='text'
-                className='form__field'
-                placeholder='Enter your name'
-              ></input>
-            </label>
-            <div className='form__radio__main-container'>
-              <div className='form__radio-container'>
-                <label className='form__label__radio'>
-                  <input type='radio' className='form__radio'></input>
-                  <span className='form__label__radio__checkmark'></span>
-                  <span className='form__label__radio__title'>Email</span>
-                </label>
-                <label className='form__label__radio'>
-                  <input type='radio' className='form__radio'></input>
-                  <span className='form__label__radio__checkmark'></span>
-                  <span className='form__label__radio__title'>Telegram</span>
-                </label>
-              </div>
-              <input type='email' className='form__field' placeholder='Enter your email'></input>
-            </div>
-            <label className='form__label'>
-              <span className='form__label__title'>Enter your phone number</span>
-              <input
-                type='text'
-                className='form__field'
-                placeholder='+30 000 000 00 00'
-              ></input>
-            </label>
-            <button className='form__button page__button'>
-              Get consultation
-            </button>
-          </div>
-        </div>
-      </form>
     </section>
   )
 }
