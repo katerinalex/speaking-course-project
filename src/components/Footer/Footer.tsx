e2ad57939805d1bf93dcdd1bec5b8b33824a4274
Olecsiy <infernoregent@gmail.com>
2024-06-26 14:47:24 +0300

    Resolving conflicts and making changes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,57 @@ import './footer.scss';
 
 export const Footer = () => {
   return (
-    <footer className='footer'>
+    <>
+          <form className='form'>
+        <div className='form-container'>
+          <h1 className='form__title'>Do you need additional consultation?</h1>
+          <div className='form__field__main-container'>
+          <div className='form__field-container'>
+            <label className='form__label form__label-first'>
+              <span className='form__label__title'>Name</span>
+              <input
+                type='text'
+                className='form__field'
+                placeholder='Enter your name'
+              ></input>
+            </label>
+            <div className='form__label__radio-button__main-container'>
+              <div className='form__label__radio-button-container'>
+                <label className='form__label__radio'>
+                  <input type='checkbox' className='form__label__radio-button'></input>
+                  <span className='form__label__radio__checkmark'></span>
+                  <span className='form__label__radio__title'>Email</span>
+                </label>
+                <label className='form__label__radio'>
+                  <input type='checkbox' className='form__label__radio-button'></input>
+                  <span className='form__label__radio__checkmark'></span>
+                  <span className='form__label__radio__title'>Telegram</span>
+                </label>
+              </div>
+              <input type='email' className='form__field' placeholder='Enter your email'></input>
+            </div>
+            <label className='form__label form__label-second'>
+              <span className='form__label__title'>Enter your phone number</span>
+              <input
+                type='text'
+                className='form__field'
+                placeholder='+30 000 000 00 00'
+              ></input>
+            </label>
+            <button className='form__button form__button_grid page__button'>
+              Get consultation
+            </button>
+          </div>
+          <label className='form__label form__label__check'>
+            <input type='checkbox' className='form__label__check-radio'/>
+            <span className='form__label__check-radio__mark' />
+            I consent to the processing of my personal data
+          </label>
+          </div>
+        </div>
+      </form>
+      <footer className='footer'>
+      
       <img src="images/logo-img.svg" className='footer-logo' />
       <div className='footer-container'>
         <ul className="footer__list">
@@ -66,5 +116,6 @@ export const Footer = () => {
       </div>
       <h3 className='footer__title footer__title-copyright'><img src='images/quotation.svg' />Public speaking courses by Anastasiia Shkiria, 2024 All rights reserved</h3>
     </footer>
+    </>
   )
 }
---
+++ src/components/SuccessPage/SuccessPage.tsx
@@ -0,0 +1,263 @@
+import "./success.scss";
+
+/* eslint-disable jsx-a11y/alt-text */
+
+export const SuccessPage = () => {
+  return (
+    <section className="success">
+      <h1 className="page__title success__main-title">History of success</h1>
+      <div className="success-container success-container-desktop  success-container-desktop-reverse success-container-desktop-first">
+        <div>
+          <h2 className="page__title_medium success__title_medium">Success</h2>
+          <picture>
+            <source
+              media="(min-width: 1440px)"
+              srcSet="images/success-main-img-desktop.png"
+            />
+            <source
+              media="(min-width: 640px)"
+              srcSet="images/success-main-img-tablet.png"
+            />
+            <img src="images/success-main-img.png" className="success-img" />
+          </picture>
+        </div>
+        <div className="success-container-article-desktop">
+          <h2 className="success__title">Anna Havrylenko</h2>
+          <p className="page__paragraph success__paragraph">
+            When I heard that Nastia was launching the Public Speaking course, I
+            realised that it was exactly what I needed. I used a lot of
+            parasitic words, had too fast a speech rate, and because of this, I
+            didn't have enough air during my speeches and my voice started to
+            tremble. <br /> <br />
+            From the first lesson, I was glad I joined. Nastia shared her life
+            hacks and taught me cool breathing and stress reduction techniques.
+            We analysed the structure of a report, learnt how to make pauses and
+            accents, and how to maintain eye contact. <br /> <br />
+            Nastya constantly encouraged us to step out of our comfort zone and
+            work on ourselves. It was difficult: I rewrote my speeches dozens of
+            times, constantly practised my breathing and diction, and tried my
+            best to speak more slowly. <br /> <br />
+            But all these efforts proved to be worthwhile, as I became more
+            confident, minimised the use of parasitic words, and finally figured
+            out how to keep the right pace and pause. <br /> <br />
+            I saw not only my own progress, but also that of my fellow students.
+            I sincerely rejoiced at their success and admired everyone's
+            performances. <br /> <br />
+            I am sincerely grateful to Nastya for her faith in our strength and
+            support. It was an incredible experience that helped me become
+            better and feel confident in my abilities. <br /> <br />
+            The changes I have suggested are mainly related to punctuation and
+            small stylistic corrections. Your writing style is quite good and
+            reflects your thoughts in a clear and emotional way.
+          </p>
+          <div className="success-container-links">
+            <img src="images/telegram-logo-black.svg" />
+            <img src="images/instagram-icon-secondary-img.svg" />
+          </div>
+        </div>
+      </div>
+      <div className="success-container success-container-desktop">
+        <div>
+          <picture>
+            <source
+              media="(min-width: 1440px)"
+              srcSet="images/tania-img-desktop.png"
+            />
+            <source
+              media="(min-width: 640px)"
+              srcSet="images/tania-img-tablet.png"
+            />
+            <img src="images/tania-img.png" className="success-img" />
+          </picture>
+        </div>
+        <div className="success-container-article-desktop">
+          <h2 className="success__title">Tania Melnychuk</h2>
+          <p className="page__paragraph success__paragraph">
+            I want to thank Anastasiia for her course and personal mentoring,
+            the difference is immediately noticeable. First and foremost, my
+            voice has stopped trembling; previously, I would have shortness of
+            breath and make odd sounds during conversations when I ran out of
+            air, but now I know how to deal with it and how to improve even
+            more.
+            <br /> <br />
+            And my greatest achievement is EMOTIONS. It's laughable how anger,
+            sadness, and joy used to be all in the same tone, but now I express
+            them so vividly! And I consider this a success. <br /> <br />
+            It's very important that I've become psychologically more open and
+            confident. A special thanks for the meditations (I love them so
+            much) and for the jokes that lightened the mood. <br /> <br />
+            I sincerely thank Anastasiia for this course and for being a part of
+            my life, it's a real reward, sending you a hug. <br /> <br />
+            Guys, I can recommend Nastia's course with great respect and
+            confidence. Beyond the knowledge and extensive practice, you'll gain
+            awesome people in your circle, jokes, support, and self-discovery.{" "}
+          </p>
+          <div className="success-container-links">
+            <img src="images/telegram-logo-black.svg" />
+            <img src="images/instagram-icon-secondary-img.svg" />
+          </div>
+        </div>
+      </div>
+      <div className="success-container success-container-desktop success-container-desktop-reverse success-container-flex">
+        <div>
+          <picture>
+            <source
+              media="(min-width: 1440px)"
+              srcSet="images/hlib-img-desktop.png"
+            />
+            <source
+              media="(min-width: 640px)"
+              srcSet="images/hlib-img-tablet.png"
+            />
+            <img src="images/hlib-img.png" className="success-img success-img" />
+          </picture>
+        </div>
+        <div className="success-container-article success-container-article-desktop">
+          <h2 className="success__title success__title-flex">Hlib Savinov</h2>
+          <p className="page__paragraph success__paragraph">
+            I highly recommend Anastasiia's public speaking courses. Before
+            starting the course, I had issues with self-esteem and fears of
+            speaking in front of groups (for me, a group was already from two
+            people). However, thanks to the courses, I realized that I needed to
+            confront these difficulties.
+            <br /> <br />
+            Most importantly, I learned how to do this and now I'm applying
+            these skills in practice. It's significant that the courses don't
+            force you to overcome your fears; instead, through group sessions,
+            you naturally begin to want to open up and be heard!
+            <br /> <br />
+            If you have the same fears as I did, give it a try - I'm confident
+            you'll understand me after the course. <br /> <br />
+          </p>
+          <div className="success-container-links">
+            <img src="images/telegram-logo-black.svg" />
+            <img src="images/instagram-icon-secondary-img.svg" />
+          </div>
+        </div>
+      </div>
+      <div className="success-container success-container-desktop success-container-desktop-reverse success-container-flex success-container-flex_conversely">
+        <div>
+          <picture>
+            <source
+              media="(min-width: 1440px)"
+              srcSet="images/yulia-img-desktop.png"
+            />
+            <source
+              media="(min-width: 640px)"
+              srcSet="images/yulia-img-tablet.png"
+            />
+            <img src="images/yulia-img.png" className="success-img" />
+          </picture>
+        </div>
+        <div className="success-container-article success-container-article-desktop">
+          <h2 className="success__title success__title-flex">Yuliia Hladkevych</h2>
+          <p className="page__paragraph success__paragraph">
+            I was part of the first cohort of Nastia’s course, and I can say
+            with certainty that it’s well worth the attention, and I’d love to
+            go through it again. <br /> <br />
+            Nastia is a true professional in her field. She will inspire you to
+            speak differently. <br /> <br />
+            When I was studying, I often spoke publicly and enjoyed it. But then
+            the need for it faded, as this skill became unnecessary in my job.
+            And now, as I'm learning again, the fear of public speaking has
+            returned. The coolest thing Anastasiia does in her course is to help
+            "draw out" your voice. And you begin to speak <br /> <br />I
+            recorded the first video about 20 times, and the last one in one go.
+            The course will help you to be heard, and in today’s world of social
+            networks, your voice can work wonders. And Anastasiia in this course
+            will help you achieve that.
+          </p>
+          <div className="success-container-links">
+            <img src="images/telegram-logo-black.svg" />
+            <img src="images/instagram-icon-secondary-img.svg" />
+          </div>
+        </div>
+      </div>
+      <div className="success-container success-container-desktop">
+        <div>
+          <picture>
+            <source
+              media="(min-width: 1440px)"
+              srcSet="images/arthur-img-desktop.png"
+            />
+            <source
+              media="(min-width: 640px)"
+              srcSet="images/arthur-img-tablet.png"
+            />
+            <img src="images/arthur-img.png" className="success-img" />
+          </picture>
+        </div>
+        <div className="success-container-article-desktop">
+          <h2 className="success__title">Arthur Pshenko</h2>
+          <p className="page__paragraph success__paragraph">
+            At the start, I genuinely thought I would just forget all my words,
+            or worse, become as dull to the audience as an old history lecture.
+            And honestly, it became tiring to think about how to avoid the
+            pitfalls of filler words like "um" and "uh". <br /> <br />
+            But thanks to the course, I gained many interesting tips that helped
+            me manage stress and elevate my speaking level. For instance, I
+            mastered the skill of using pauses, highlighting key points, and
+            structuring my presentations, along with many other useful
+            techniques.
+            <br /> <br />
+            I also enjoyed the practical assignments in the format of online
+            meetings, where we analyzed different speakers' performances,
+            discussed their techniques, and interacted with other course
+            participants. Working in pairs was also very engaging. <br /> <br />
+            Now, I can happily say that I express my thoughts more clearly, my
+            Ukrainian has improved, and I know what's important in my
+            presentations. I hardly have issues with filler words anymore — now
+            I use pauses effectively and highlight the main points. Of course,
+            there's always room to grow, especially in writing structured
+            reports and controlling the tempo of speech, but I'm confident that
+            I'll master these as well! <br /> <br />A big thank you to
+            Anastasiia, she truly helped me in this process. And I recommend
+            this course to anyone who wants to become a better speaker and
+            perform in front of an audience.
+          </p>
+          <div className="success-container-links">
+            <img src="images/telegram-logo-black.svg" />
+            <img src="images/instagram-icon-secondary-img.svg" />
+          </div>
+        </div>
+      </div>
+      <div className="success-container success-container-desktop success-container-desktop-reverse">
+        <div>
+          <picture>
+            <source
+              media="(min-width: 1440px)"
+              srcSet="images/anna-img-desktop.png"
+            />
+            <source
+              media="(min-width: 640px)"
+              srcSet="images/anna-img-tablet.png"
+            />
+            <img src="images/anna-img.png" className="success-img"/>
+          </picture>
+        </div>
+        <div className="success-container-article-desktop">
+          <h2 className="success__title">Anna Pastushenko</h2>
+          <p className="page__paragraph success__paragraph">
+            Before taking the course, I felt quite insecure when delivering
+            speeches, especially when it came to starting them. <br /> <br />
+            FDuring the course, I learned how to manage my fear, received and
+            began to successfully apply Anastasiia's advice on engaging with the
+            audience, and focused on personal development first and foremost.{" "}
+            <br /> <br />
+            What I particularly enjoyed was the interaction during our sessions,
+            the interactivity, exercises, and interesting methods to work
+            through my own difficulties. <br /> <br />
+            Now, I can confidently say that I find it quite easy to compile
+            information for an engaging speech, present it in a unique,
+            captivating format, and handle the nerves and insecurity during
+            speeches.
+          </p>
+          <div className="success-container-links">
+            <img src="images/telegram-logo-black.svg" />
+            <img src="images/instagram-icon-secondary-img.svg" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
